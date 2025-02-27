dd35bff38dc8384459ce86fbfdfd2e2f19df7204
hopeAlexis <dododor.mistt@gmail.com>
2025-02-28 00:37:15 +0200

    [chess] uploaded 3 lab

--- booklist-app/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.booklistapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.goalsApp to javafx.fxml;
-    exports com.example.goalsApp;
-}
